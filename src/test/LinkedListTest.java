de9a2b0aeae74beac1af1c9b977c0684feb67515
tulXoro <tulxoro@hotmail.com>
2021-01-08 15:59:40 -0800

    Added 2 more tests

--- src/test/LinkedListTest.java
+++ src/test/LinkedListTest.java
@@ -4,15 +4,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LinkedListTest {
     @Test
-    public void LinkedListTestAdd1() {
+    public void LLTAdd1() {
         LinkedList<String> list = new LinkedList<>();
         list.add("Test");
         assertEquals("Test", list.at(0));
     }
 
     @Test
-    public void LinkedListTestAdd2() {
+    public void LLTAdd2() {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Test");
+        list.add("Foo");
+        assertEquals("Foo", list.at(1));
+    }
+
+    @Test
+    public void LLTAddException1() {
         LinkedList<String> list = new LinkedList<>();
         assertThrows(IndexOutOfBoundsException.class, () -> list.at(0));
+    }
+
+    @Test
+    public void LLTAddException2() {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("foo");
+        assertThrows(IndexOutOfBoundsException.class, () -> list.at(1));
     }
 }
