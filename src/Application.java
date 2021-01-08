c2176f172bc95614493a83801ecb469ca22288d8
tulXoro <tulxoro@hotmail.com>
2021-01-07 22:40:56 -0800

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,13 @@
+public class Application {
+
+    public final String title = "Data Structures Visualized";
+    public static final int WIDTH = 640, HEIGHT = WIDTH/12 * 9; //used for pop out window
+
+    public Application() {
+        new Window(WIDTH,HEIGHT,title,this);
+    }
+
+    public static void main(String[] args) {
+        new Application();
+    }
+}
---
+++ src/Window.java
@@ -0,0 +1,24 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Window extends Canvas{
+
+    private static final long serialVersionUID = 6596273530124848110L;
+
+    public Window(int width, int height, String title, Application app) {
+
+        JFrame frame = new JFrame(title);
+
+        frame.setPreferredSize(new Dimension(width,height));
+        frame.setMaximumSize(new Dimension(width,height));
+        frame.setMinimumSize(new Dimension(width,height));
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //closes game when closed
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+}
