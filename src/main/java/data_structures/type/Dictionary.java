33fc24d9c41efc808fd2637756333ede095e0a6d
tulXoro <tulxoro@hotmail.com>
2021-01-08 13:12:32 -0800

    Documented

--- src/main/java/data_structures/type/Dictionary.java
+++ src/main/java/data_structures/type/Dictionary.java
@@ -1,5 +1,10 @@
 package main.java.data_structures.type;
 
+/**
+ * Represents a dictionary
+ * @param <K> key value
+ * @param <V> value to be stored
+ */
 public interface Dictionary<K, V> {
     int size();
     K add(K key, V value);
