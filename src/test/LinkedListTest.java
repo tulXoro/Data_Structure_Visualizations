e7524386b5cc707de4f8577d71e8378b6a5354d4
DoubleAPowerBox <55107564+DoubleAPowerBox@users.noreply.github.com>
2021-01-10 18:44:36 -0800

    Integer tests for linked list.

--- src/test/LinkedListTest.java
+++ src/test/LinkedListTest.java
@@ -19,6 +19,21 @@ public class LinkedListTest {
     }
 
     @Test
+    public void LLTAdd3() {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(134);
+        list.add(4234);
+        list.add(-864);
+        list.add(734);
+        list.add(95);
+        assertEquals(134, list.at(0));
+        assertEquals(4234, list.at(1));
+        assertEquals(-864, list.at(2));
+        assertEquals(734, list.at(3));
+        assertEquals(95, list.at(4));
+    }
+
+    @Test
     public void LLTAddException1() {
         LinkedList<String> list = new LinkedList<>();
         assertThrows(IndexOutOfBoundsException.class, () -> list.at(0));
